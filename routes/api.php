9b1ca101c537d0486b05293ba2ed3d6859673ab8
Arianfh <ariansyahfadilah@gmail.com>
2023-10-01 08:24:58 +0700

    tambah api AuthController dan TodoController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix' => 'auth'], function(){
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::group(['middleware' => 'auth:api'], function(){
+        Route::get('info', [AuthController::class, 'info']);
+        Route::post('logout', [AuthController::class, 'logout']);
+    });
+});
+
+Route::group(['prefix' => 'todo'], function(){
+    Route::group(['middleware' => 'auth:api'], function(){
+        Route::get('get', [TodoController::class, 'getTodoList']);
+        Route::post('add', [TodoController::class, 'createTodo']);
+        Route::get('get_id/{id}', [TodoController::class, 'getTodoId']);
+        Route::delete('delete/{id}', [TodoController::class, 'deleteTodo']);
+        Route::put('update/{id}', [TodoController::class, 'updateTodo']);
+    });
 });
